65e444e4e6cae53590b7ad8cac4d09368c38603a
Aadil Anil Kumar <aadilanilkumar@gmail.com>
2017-10-13 22:37:29 +0200

    Changed tabs.html and tabs.ts to include Games and Create pages.

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -6,6 +6,10 @@ import { FeedPage } from '../feed/feed';
 
 import { AchievementsPage } from '../achievements/achievements'
 
+import { GamesPage } from '../games/games'
+
+import { CreatePage } from '../create/create'
+
 @Component({
   templateUrl: 'tabs.html'
 })
@@ -14,6 +18,8 @@ export class TabsPage {
   tab1Root = DashboardPage;
   tab2Root = FeedPage;
   tab3Root = AchievementsPage;
+  tab4Root = GamesPage; 
+  tab5Root = CreatePage; 
 
   constructor() {
 
