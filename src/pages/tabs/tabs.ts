7c05d03d4a86ebfed966cc05f4af36675210afa8
Aadil Anil Kumar <aadilanilkumar@gmail.com>
2017-10-13 22:28:23 +0200

    Changed tab.ts to inlcude FeedPage and AchievementsPage

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 
 import { DashboardPage } from '../dashboard/dashboard';
 
+import { FeedPage } from '../feed/feed';
+
+import { AchievementsPage } from '../achievements/achievements'
+
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
   tab1Root = DashboardPage;
-  tab2Root = DashboardPage;
-  tab3Root = DashboardPage;
+  tab2Root = FeedPage;
+  tab3Root = AchievementsPage;
 
   constructor() {
 
