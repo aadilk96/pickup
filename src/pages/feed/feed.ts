3b82e3b439cdfa640232a49af5278926e0a0189f
Aadil Anil Kumar <aadilanilkumar@gmail.com>
2017-10-14 05:50:26 +0200

    Added infinite loop to feed page

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -7,8 +7,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'feed.html',
 })
 export class FeedPage {
+  items = []; 
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    for (var i = 0; i < 30; i++){
+      this.items.push( this.items.length );
+
+    }
+  }
+  doInfinite(): Promise<any> {
+    console.log('Begin async operation');
+
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        for (var i = 0; i < 30; i++) {
+          this.items.push( this.items.length );
+        }
+
+        console.log('Async operation has ended');
+        resolve();
+      }, 500);
+    })
   }
 
   ionViewDidLoad() {
