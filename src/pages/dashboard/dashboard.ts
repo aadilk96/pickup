8de519d98146eb012a2ed90b74978e9fa00b2506
Steven Abreu <stevenabreu7@gmail.com>
2017-10-14 07:21:23 +0200

    + add mock data in dashboard

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -4,6 +4,16 @@ import { AuthProvider } from '../../providers/auth/auth';
 import { DatabaseProvider } from '../../providers/database/database';
 import { WelcomePage } from '../welcome/welcome';
 
+export class User {
+  firstName: string;
+  lastName: string;
+  displayName: string;
+  email: string;
+  birthday: string;
+  joindate: string;
+  uid: string;
+}
+
 @IonicPage()
 @Component({
   selector: 'page-dashboard',
@@ -12,6 +22,16 @@ import { WelcomePage } from '../welcome/welcome';
 export class DashboardPage {
 
   User = "Account";
+
+  user: User = {
+    firstName: 'adolf',
+    lastName: 'hitler',
+    displayName: 'nazi001',
+    email: 'nazi@germany.de',
+    birthday: '01/01/1909',
+    joindate: '10/10/2017',
+    uid: 'wfwf3849f3h'
+  }
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private auth: AuthProvider, private db: DatabaseProvider) {  
   }
