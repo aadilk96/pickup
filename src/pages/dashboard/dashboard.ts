79affffabcef7e4d86adaa585aa9bec0871fb1e1
Steven Abreu <stevenabreu7@gmail.com>
2017-10-14 10:50:21 +0200

    + BS

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AuthProvider } from '../../providers/auth/auth';
 import { DatabaseProvider } from '../../providers/database/database';
@@ -23,7 +23,8 @@ export class User {
   templateUrl: 'dashboard.html',
 })
 export class DashboardPage {
-  @ViewChild('lineCanvas') lineCanvas;
+  @ViewChild('lineCanvas')
+  public lineCanvas: ElementRef;
   lineChart: any;
 
   User = "Account";
@@ -38,6 +39,37 @@ export class DashboardPage {
   }
 
   constructor(public alertCtrl: AlertController) {
+    console.log(this.lineCanvas);
+    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+      type: 'line',
+      data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+          {
+            label: "My First dataset",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40],
+            spanGaps: false,
+          }
+        ]
+      }
+    });
   }
 
   showPromptFirstName() {
