50dbcb76c10fef5d5102b94d9a2b6f9011af65f2
Aadil Anil Kumar <aadilanilkumar@gmail.com>
2017-10-14 10:44:34 +0200

    courthierarcy

---
+++ src/pages/courthierarchy/courthierarchy.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { CourthierarchyPage } from './courthierarchy';
+
+@NgModule({
+  declarations: [
+    CourthierarchyPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(CourthierarchyPage),
+  ],
+})
+export class CourthierarchyPageModule {}
